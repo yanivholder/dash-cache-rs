2c46f168f8d229c21a7a698d5bafe274b38ff9cd
Yaniv Holder <yaniv.holder@outlook.com>
2024-02-18 08:00:18 +0200

    Add associative cache

--- src/associative_cache/associative_cache_settings.rs
+++ src/associative_cache/associative_cache_settings.rs
@@ -1,1 +1,18 @@
+use crate::shared::settings::EvictionPolicy;
 
+#[derive(Debug, Clone)]
+pub struct AssociativeCacheSettings {
+	pub size: usize,
+	pub segment_size: usize,
+	pub bucket_size: usize,
+	pub eviction_policy: EvictionPolicy,
+	pub debug_mode: u8,
+}
+
+pub const DEFAULT_SETTINGS: AssociativeCacheSettings = AssociativeCacheSettings {
+	size: 1,
+	segment_size: 1,
+	bucket_size: 512,
+	eviction_policy: EvictionPolicy::Lru,
+	debug_mode: 0,
+};
