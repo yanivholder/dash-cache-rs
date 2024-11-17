af923eb2c76e088f66982aa8be1934365fc981ae
Yaniv Holder <yaniv.holder@outlook.com>
2024-11-17 14:10:38 +0200

    Change JNI so several cache could run in caffeine (#14)

--- src/associative_cache/associative_cache_jni.rs
+++ src/associative_cache/associative_cache_jni.rs
@@ -1,46 +1,46 @@
-use super::associative_cache_settings::DEFAULT_SETTINGS;
-use super::AssociativeCache;
-use crate::shared::traits::cache::Cache;
-use jni::{objects::JClass, sys::jlong, JNIEnv};
-use once_cell::sync::OnceCell;
+// use super::associative_cache_settings::DEFAULT_SETTINGS;
+// use super::AssociativeCache;
+// use crate::shared::traits::cache::Cache;
+// use jni::{objects::JClass, sys::jlong, JNIEnv};
+// use once_cell::sync::OnceCell;
 
-type DefaultAssociativeCache = AssociativeCache<i64, i64>;
+// type DefaultAssociativeCache = AssociativeCache<i64, i64>;
 
-static mut CACHE: OnceCell<DefaultAssociativeCache> = OnceCell::new();
+// static mut CACHE: OnceCell<DefaultAssociativeCache> = OnceCell::new();
 
-fn shared_cache() -> &'static mut DefaultAssociativeCache {
-	unsafe { CACHE.get_mut().expect("The cache is not initialized") }
-}
+// fn shared_cache() -> &'static mut DefaultAssociativeCache {
+// 	unsafe { CACHE.get_mut().expect("The cache is not initialized") }
+// }
 
-#[no_mangle]
-pub extern "system" fn Java_com_github_benmanes_caffeine_cache_simulator_policy_associativeCacheRust_AssociativeCacheRust_initCache(
-	_env: JNIEnv,
-	_class: JClass,
-) {
-	unsafe {
-		CACHE.set(AssociativeCache::new(DEFAULT_SETTINGS)).expect("");
-	}
-}
+// #[no_mangle]
+// pub extern "system" fn Java_com_github_benmanes_caffeine_cache_simulator_policy_associativeCacheRust_AssociativeCacheRust_initCache(
+// 	_env: JNIEnv,
+// 	_class: JClass,
+// ) {
+// 	unsafe {
+// 		CACHE.set(AssociativeCache::new(DEFAULT_SETTINGS)).expect("");
+// 	}
+// }
 
-#[no_mangle]
-pub extern "system" fn Java_com_github_benmanes_caffeine_cache_simulator_policy_associativeCacheRust_AssociativeCacheRust_getFromCacheIfPresent(
-	_env: JNIEnv,
-	_class: JClass,
-	key: jlong,
-) -> jlong {
-	let res = shared_cache().get(&key);
-	match res {
-		None => -1,
-		Some(value) => value,
-	}
-}
+// #[no_mangle]
+// pub extern "system" fn Java_com_github_benmanes_caffeine_cache_simulator_policy_associativeCacheRust_AssociativeCacheRust_getFromCacheIfPresent(
+// 	_env: JNIEnv,
+// 	_class: JClass,
+// 	key: jlong,
+// ) -> jlong {
+// 	let res = shared_cache().get(&key);
+// 	match res {
+// 		None => -1,
+// 		Some(value) => value,
+// 	}
+// }
 
-#[no_mangle]
-pub extern "system" fn Java_com_github_benmanes_caffeine_cache_simulator_policy_associativeCacheRust_AssociativeCacheRust_putToCache(
-	_env: JNIEnv,
-	_class: JClass,
-	key: jlong,
-	value: jlong,
-) {
-	shared_cache().put(key, value);
-}
+// #[no_mangle]
+// pub extern "system" fn Java_com_github_benmanes_caffeine_cache_simulator_policy_associativeCacheRust_AssociativeCacheRust_putToCache(
+// 	_env: JNIEnv,
+// 	_class: JClass,
+// 	key: jlong,
+// 	value: jlong,
+// ) {
+// 	shared_cache().put(key, value);
+// }
--- src/associative_cache/mod.rs
+++ src/associative_cache/mod.rs
@@ -1,150 +1,150 @@
-use crate::shared::item::Item;
-use crate::shared::settings::EvictionPolicy;
-use crate::shared::traits::bucket::Bucket;
-use crate::shared::traits::cache::Cache;
-use crate::shared::traits::segment::Segment;
-use associative_cache_settings::AssociativeCacheSettings;
-use std::hash::Hash;
+// use crate::shared::item::Item;
+// use crate::shared::settings::EvictionPolicy;
+// use crate::shared::traits::bucket::Bucket;
+// use crate::shared::traits::cache::Cache;
+// use crate::shared::traits::segment::Segment;
+// use associative_cache_settings::AssociativeCacheSettings;
+// use std::hash::Hash;
 
-pub mod associative_cache_jni;
-mod associative_cache_settings;
+// pub mod associative_cache_jni;
+// mod associative_cache_settings;
 
-#[derive(Debug)]
-pub struct AssociativeCache<K, V>
-where
-	K: Hash + Eq + Copy,
-	V: Eq + Copy,
-{
-	pub segments: Vec<AssociativeCacheSegment<K, V>>,
-}
+// #[derive(Debug)]
+// pub struct AssociativeCache<K, V>
+// where
+// 	K: Hash + Eq + Copy,
+// 	V: Eq + Copy,
+// {
+// 	pub segments: Vec<AssociativeCacheSegment<K, V>>,
+// }
 
-impl<K, V> AssociativeCache<K, V>
-where
-	K: Hash + Eq + Copy,
-	V: Eq + Copy,
-{
-	pub fn new(settings: AssociativeCacheSettings) -> Self {
-		let mut segments = Vec::new();
-		for _ in 0..settings.size {
-			segments.push(AssociativeCacheSegment::new(settings.clone()));
-		}
-		Self { segments }
-	}
-}
+// impl<K, V> AssociativeCache<K, V>
+// where
+// 	K: Hash + Eq + Copy,
+// 	V: Eq + Copy,
+// {
+// 	pub fn new(settings: AssociativeCacheSettings) -> Self {
+// 		let mut segments = Vec::new();
+// 		for _ in 0..settings.size {
+// 			segments.push(AssociativeCacheSegment::new(settings.clone()));
+// 		}
+// 		Self { segments }
+// 	}
+// }
 
-impl<K, V> Cache<K, V> for AssociativeCache<K, V>
-where
-	K: Hash + Eq + Copy,
-	V: Eq + Copy,
-{
-	type S = AssociativeCacheSegment<K, V>;
+// impl<K, V> Cache<K, V> for AssociativeCache<K, V>
+// where
+// 	K: Hash + Eq + Copy,
+// 	V: Eq + Copy,
+// {
+// 	type S = AssociativeCacheSegment<K, V>;
 
-	fn get_segments_mut(&mut self) -> &mut Vec<Self::S> {
-		&mut self.segments
-	}
-}
+// 	fn get_segments_mut(&mut self) -> &mut Vec<Self::S> {
+// 		&mut self.segments
+// 	}
+// }
 
-#[derive(Debug)]
-pub struct AssociativeCacheSegment<K, V>
-where
-	K: Hash + Eq + Copy,
-	V: Eq + Copy,
-{
-	pub buckets: Vec<AssociativeCacheBucket<K, V>>,
-	pub segment_size: usize,
-}
+// #[derive(Debug)]
+// pub struct AssociativeCacheSegment<K, V>
+// where
+// 	K: Hash + Eq + Copy,
+// 	V: Eq + Copy,
+// {
+// 	pub buckets: Vec<AssociativeCacheBucket<K, V>>,
+// 	pub segment_size: usize,
+// }
 
-impl<K, V> AssociativeCacheSegment<K, V>
-where
-	K: Hash + Eq + Copy,
-	V: Eq + Copy,
-{
-	pub fn new(settings: AssociativeCacheSettings) -> Self {
-		let mut buckets = Vec::new();
-		for _ in 0..settings.segment_size {
-			buckets.push(AssociativeCacheBucket::new(settings.clone()));
-		}
-		Self {
-			buckets,
-			segment_size: settings.segment_size,
-		}
-	}
-}
+// impl<K, V> AssociativeCacheSegment<K, V>
+// where
+// 	K: Hash + Eq + Copy,
+// 	V: Eq + Copy,
+// {
+// 	pub fn new(settings: AssociativeCacheSettings) -> Self {
+// 		let mut buckets = Vec::new();
+// 		for _ in 0..settings.segment_size {
+// 			buckets.push(AssociativeCacheBucket::new(settings.clone()));
+// 		}
+// 		Self {
+// 			buckets,
+// 			segment_size: settings.segment_size,
+// 		}
+// 	}
+// }
 
-impl<K, V> Segment<K, V> for AssociativeCacheSegment<K, V>
-where
-	K: Hash + Eq + Copy,
-	V: Eq + Copy,
-{
-	type B = AssociativeCacheBucket<K, V>;
+// impl<K, V> Segment<K, V> for AssociativeCacheSegment<K, V>
+// where
+// 	K: Hash + Eq + Copy,
+// 	V: Eq + Copy,
+// {
+// 	type B = AssociativeCacheBucket<K, V>;
 
-	fn get_buckets(&self) -> &Vec<Self::B> {
-		&self.buckets
-	}
+// 	fn get_buckets(&self) -> &Vec<Self::B> {
+// 		&self.buckets
+// 	}
 
-	fn get_buckets_mut(&mut self) -> &mut Vec<Self::B> {
-		&mut self.buckets
-	}
+// 	fn get_buckets_mut(&mut self) -> &mut Vec<Self::B> {
+// 		&mut self.buckets
+// 	}
 
-	fn get_segment_size(&self) -> usize {
-		self.segment_size
-	}
-}
+// 	fn get_segment_size(&self) -> usize {
+// 		self.segment_size
+// 	}
+// }
 
-#[derive(Debug)]
-pub struct AssociativeCacheBucket<K, V>
-where
-	K: Hash + Eq + Copy,
-	V: Eq + Copy,
-{
-	items: Vec<Item<K, V>>,
-	max_size: usize,
-	eviction_policy: EvictionPolicy,
-}
+// #[derive(Debug)]
+// pub struct AssociativeCacheBucket<K, V>
+// where
+// 	K: Hash + Eq + Copy,
+// 	V: Eq + Copy,
+// {
+// 	items: Vec<Item<K, V>>,
+// 	max_size: usize,
+// 	eviction_policy: EvictionPolicy,
+// }
 
-impl<K, V> AssociativeCacheBucket<K, V>
-where
-	K: Hash + Eq + Copy,
-	V: Eq + Copy,
-{
-	pub fn new(settings: AssociativeCacheSettings) -> Self {
-		Self {
-			items: Vec::new(),
-			max_size: settings.bucket_size,
-			eviction_policy: settings.eviction_policy,
-		}
-	}
-}
+// impl<K, V> AssociativeCacheBucket<K, V>
+// where
+// 	K: Hash + Eq + Copy,
+// 	V: Eq + Copy,
+// {
+// 	pub fn new(settings: AssociativeCacheSettings) -> Self {
+// 		Self {
+// 			items: Vec::new(),
+// 			max_size: settings.bucket_size,
+// 			eviction_policy: settings.eviction_policy,
+// 		}
+// 	}
+// }
 
-impl<K, V> Bucket<K, V> for AssociativeCacheBucket<K, V>
-where
-	K: Hash + Eq + Copy,
-	V: Eq + Copy,
-{
-	fn get_items(&self) -> &Vec<Item<K, V>> {
-		&self.items
-	}
+// impl<K, V> Bucket<K, V> for AssociativeCacheBucket<K, V>
+// where
+// 	K: Hash + Eq + Copy,
+// 	V: Eq + Copy,
+// {
+// 	fn get_items(&self) -> &Vec<Item<K, V>> {
+// 		&self.items
+// 	}
 
-	fn get_items_mut(&mut self) -> &mut Vec<Item<K, V>> {
-		&mut self.items
-	}
+// 	fn get_items_mut(&mut self) -> &mut Vec<Item<K, V>> {
+// 		&mut self.items
+// 	}
 
-	fn get_max_size(&self) -> usize {
-		self.max_size
-	}
+// 	fn get_max_size(&self) -> usize {
+// 		self.max_size
+// 	}
 
-	fn get_eviction_policy(&self) -> &EvictionPolicy {
-		&self.eviction_policy
-	}
+// 	fn get_eviction_policy(&self) -> &EvictionPolicy {
+// 		&self.eviction_policy
+// 	}
 
-	fn get_and_update_lru_item(&mut self, position: usize) -> &Item<K, V> {
-		let item = self.items.remove(position);
-		self.items.push(item);
-		self.items.last().unwrap()
-	}
+// 	fn get_and_update_lru_item(&mut self, position: usize) -> &Item<K, V> {
+// 		let item = self.items.remove(position);
+// 		self.items.push(item);
+// 		self.items.last().unwrap()
+// 	}
 
-	fn evict_lru_item(&mut self) -> Option<Item<K, V>> {
-		// TODO: this is in O(n). there could be a more performant way to do that
-		Some(self.items.remove(0))
-	}
-}
+// 	fn evict_lru_item(&mut self) -> Option<Item<K, V>> {
+// 		// TODO: this is in O(n). there could be a more performant way to do that
+// 		Some(self.items.remove(0))
+// 	}
+// }
